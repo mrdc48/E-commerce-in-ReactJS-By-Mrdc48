fc9cfee4a7496fa3c635684d99c9f287fe26ca8c
chetan <chetandhotre01@gmail.com>
2022-07-07 13:20:25 +0530

    second

--- src/sign.js
+++ src/sign.js
@@ -1,5 +1,7 @@
 import { signInWithGooglePopup, createUser } from "./firebase.utils.js";
 import React from "react";
+import Signup from "./Signup.js";
+
 function sign() {
   const Data = async () => {
     const { user } = await signInWithGooglePopup();
@@ -9,6 +11,7 @@ function sign() {
     <div>
       <p>sign in page </p>
       <button onClick={Data}> sing in </button>
+      <Signup />
     </div>
   );
 }
